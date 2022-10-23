49bbde7c373cbd72396a4091281726dfe8d22ff1
민경 <tjalsrud0101@gmail.com>
2022-10-23 11:53:06 +0900

    날짜 추가

--- src/main/java/com/example/demo/dto/ranking/RankingDto.java
+++ src/main/java/com/example/demo/dto/ranking/RankingDto.java
@@ -3,8 +3,12 @@ package com.example.demo.dto.ranking;
 import com.example.demo.domain.Member;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class RankingDto {
+    String date;
     String memberNickname;
     Long memberGrade;
     Long questionCnt;
@@ -12,6 +16,7 @@ public class RankingDto {
     int rank;
 
     public RankingDto(Member member, Long questionCnt, Long answerCnt, int rank) {
+        this.date = LocalDate.now().format(DateTimeFormatter.ofPattern("MM월dd일"));
         this.memberNickname = member.getNickname();
         this.memberGrade = member.getGrade();
         this.questionCnt = questionCnt;
