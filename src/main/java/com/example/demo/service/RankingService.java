f482bca8fb803219474db23920a0aefcc6709e75
민경 <tjalsrud0101@gmail.com>
2022-11-03 00:08:24 +0900

    fix: ranking 점수 계산

--- src/main/java/com/example/demo/service/RankingService.java
+++ src/main/java/com/example/demo/service/RankingService.java
@@ -25,31 +25,36 @@ public class RankingService {
 //    @Scheduled(cron = "* * * * * *")
     public void init() {
         rankingList = new ArrayList<>();
-        HashMap<ScoreDto, Double> hashMap = new HashMap<>();
+        HashMap<ScoreDto, Double> scoreMap = new HashMap<>();
         List<Member> memberList = memberRepository.findAll();
         long questionCnt, answerCnt, answerLike = 0L, answerDislike = 0L, durationTime;
         double score;
         ScoreDto scoreDto;
         for (Member member : memberList) {
             questionCnt = questionRepository.countByMemberId(member.getId());
+
             List<Answer> answers = answerRepository.findAllByMemberId(member.getId());
             answerCnt = answers.size();
-            for (Answer answer : answers) {
-                answerLike += answer.getLikes();
-                answerDislike += answer.getDislikes();
+            if(answerCnt > 0) {
+                for (Answer answer : answers) {
+                    answerLike += answer.getLikes();
+                    answerDislike += answer.getDislikes();
+                }
             }
+
             durationTime = member.getDurationTime();
             scoreDto = new ScoreDto(member, questionCnt, answerCnt, answerLike, answerDislike, durationTime);
-            if(answerLike > answerDislike)
-                score = questionCnt + answerCnt + (durationTime / 30.0) + ((answerLike - answerDislike) * 0.5);
-            else
-                score = questionCnt + answerCnt + (durationTime / 30.0);
-            hashMap.put(scoreDto, score);
+//            if(answerLike > answerDislike)
+//                score = questionCnt + answerCnt + (durationTime / 30.0) + ((answerLike - answerDislike) * 0.5);
+//            else
+            score = questionCnt + answerCnt + (durationTime / 30.0);
+            scoreMap.put(scoreDto, score);
         }
-        List<Map.Entry<ScoreDto, Double>> scoreList = new LinkedList<>(hashMap.entrySet());
-        scoreList.sort(Map.Entry.comparingByValue());
-        Collections.reverse(scoreList);
-        for (int i = 0; i < scoreList.size(); i++) {
+        List<Map.Entry<ScoreDto, Double>> scoreList = new LinkedList<>(scoreMap.entrySet());
+        scoreList.sort((o1, o2) -> (int) (o2.getValue() - o1.getValue()));
+
+        int size = scoreList.size();
+        for (int i = 0; i < size; i++) {
             ScoreDto scores = scoreList.get(i).getKey();
             rankingList.add(new RankingDto(scores.getMember(), scores.getQuestionCnt(), scores.getAnswerCnt(), i + 1));
         }
