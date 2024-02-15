63462777921248a1583a62bd31b2eff4298c45d6
민경 <tjalsrud0101@gmail.com>
2024-02-15 14:55:40 +0900

    feat: 내가해냄 답변 엔티티 (#100)

--- src/main/java/com/peeroreum/domain/image/QuestionImage.java
+++ src/main/java/com/peeroreum/domain/image/QuestionImage.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
-public class QuestionImage extends EntityTime {
+public class QuestionImage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
