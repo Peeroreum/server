d80a8a233e9770cb293e420e8f31e563e04dbaeb
민경 <tjalsrud0101@gmail.com>
2024-02-15 19:06:07 +0900

    feat: question repository (#101)

--- src/main/java/com/peeroreum/repository/QuestionRepository.java
+++ src/main/java/com/peeroreum/repository/QuestionRepository.java
@@ -1,14 +1,18 @@
 package com.peeroreum.repository;
 
+import com.peeroreum.domain.Member;
 import com.peeroreum.domain.Question;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findAllBySubjectAndGrade(Long subject, Long grade);
-    List<Question> findAllBySubject(Long subject);
-    List<Question> findAllByGrade(Long grade);
-    Long countByMemberId(@Param("user_id") Long memberId);
+    List<Question> findAllByGradeAndSubjectAndDetailSubjectOrderByIdDesc(Long grade, Long subject, Long detailSubject, Pageable pageable);
+    List<Question> findAllBySubjectAndGradeOrderByIdDesc(Long subject, Long grade, Pageable pageable);
+    List<Question> findAllBySubjectAndDetailSubjectOrderByIdDes(Long subject, Long detailSubject, Pageable pageable);
+    List<Question> findAllBySubjectOrderByIdDes(Long subject, Pageable pageable);
+    List<Question> findAllByGradeOrderByIdDes(Long grade, Pageable pageable);
+    List<Question> findAllByMemberOrderByIdDes(Member member, Pageable pageable);
+    List<Question> findAllByTitleAndContentContainingOrderByIdDesc(String keyword, Pageable pageable);
 }
