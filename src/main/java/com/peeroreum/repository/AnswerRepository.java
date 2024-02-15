01d02cc089aed74d1855d70ae02889d9260a08d1
민경 <tjalsrud0101@gmail.com>
2024-02-15 19:12:36 +0900

    feat: answer repository (#101)

--- src/main/java/com/peeroreum/repository/AnswerRepository.java
+++ src/main/java/com/peeroreum/repository/AnswerRepository.java
@@ -1,13 +1,15 @@
 package com.peeroreum.repository;
 
 import com.peeroreum.domain.Answer;
+import com.peeroreum.domain.Member;
+import com.peeroreum.domain.Question;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findAllByQuestionId(Long questionId);
-    List<Answer> findAllByMemberId(@Param("user_id") Long memberId);
-    Long countByQuestionId(Long questionId);
+    List<Answer> findAllByQuestionIdOrderByGroupIdAscIdAsc(Long questionId, Pageable pageable);
+    List<Answer> findAllByMember(Member member, Pageable pageable);
+    Long countAllByQuestion(Question question);
 }
