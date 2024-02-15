9b88dafa02166d2e379a466104032da4efa8a605
민경 <tjalsrud0101@gmail.com>
2024-02-15 17:30:06 +0900

    feat: bookmark question controller (#105)

---
+++ src/main/java/com/peeroreum/controller/BookmarkController.java
@@ -0,0 +1,30 @@
+package com.peeroreum.controller;
+
+import com.peeroreum.dto.response.ResponseDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    public BookmarkController(BookmarkService bookmarkService) {
+        this.bookmarkService = bookmarkService;
+    }
+
+    @PutMapping("/bookmark/question/{id}")
+    public ResponseDto createQuestionBookmark(@PathVariable Long id, Principal principal) {
+        return ResponseDto.success(bookmarkService.makeQuestionBookmark(id, principal.getName()));
+    }
+
+    @DeleteMapping("/bookmark/question/{id}")
+    public ResponseDto deleteQuestionBookmark(@PathVariable Long id, Principal principal) {
+        return ResponseDto.success(bookmarkService.cancelQuestionBookmark(id, principal.getName()));
+    }
+
+}
