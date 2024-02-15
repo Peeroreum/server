cc80c29709d749dd49efbb88870571a0bc3f8e4a
민경 <tjalsrud0101@gmail.com>
2024-02-15 15:04:55 +0900

    feat: question, answer like entity (#100)

---
+++ src/main/java/com/peeroreum/domain/like/AnswerLike.java
@@ -0,0 +1,32 @@
+package com.peeroreum.domain.like;
+
+import com.peeroreum.domain.Answer;
+import com.peeroreum.domain.EntityTime;
+import com.peeroreum.domain.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class AnswerLike extends EntityTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    AnswerLike(Member member, Answer answer) {
+        this.member = member;
+        this.answer = answer;
+    }
+}
