372f723e33262aa3b6c4eb3bc2ac694152b2c784
민경 <tjalsrud0101@gmail.com>
2024-02-15 14:58:24 +0900

    feat: question bookmark entity (#100)

---
+++ src/main/java/com/peeroreum/domain/bookmark/QuestionBookMark.java
@@ -0,0 +1,28 @@
+package com.peeroreum.domain.bookmark;
+
+import com.peeroreum.domain.EntityTime;
+import com.peeroreum.domain.Member;
+import com.peeroreum.domain.Question;
+
+import javax.persistence.*;
+
+public class QuestionBookMark extends EntityTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    QuestionBookMark(Member member, Question question) {
+        this.member = member;
+        this.question = question;
+    }
+
+}
