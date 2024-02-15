9b7b3079417b92716ccb47155cf24dc8d7a5e9c7
민경 <tjalsrud0101@gmail.com>
2024-02-15 17:28:29 +0900

    feat: like question,answer controller (#105)

---
+++ src/main/java/com/peeroreum/controller/LikeController.java
@@ -0,0 +1,40 @@
+package com.peeroreum.controller;
+
+import com.peeroreum.dto.response.ResponseDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class LikeController {
+
+    private final LikeService likeService;
+
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @PostMapping("/like/question/{id}")
+    public ResponseDto createQuestionLike(@PathVariable Long id, Principal principal) {
+        return ResponseDto.success(likeService.makeQuestionLike(id, principal.getName()));
+    }
+
+    @PostMapping("/like/answer/{id}")
+    public ResponseDto createAnswerLike(@PathVariable Long id, Principal principal) {
+        return ResponseDto.success(likeService.makeAnswerLike(id, principal.getName()));
+    }
+
+    @DeleteMapping("/like/question/{id}")
+    public ResponseDto deleteQuestionLike(@PathVariable Long id, Principal principal) {
+        return ResponseDto.success(likeService.cancelQuestionLike(id, principal.getName()));
+    }
+
+    @DeleteMapping("/like/answer/{id}")
+    public ResponseDto deleteAnswerLike(@PathVariable Long id, Principal principal) {
+        return ResponseDto.success(likeService.cancelAnswerLike(id, principal.getName()));
+    }
+
+}
