5f7c2374e20c1cb85a17c0496c8a0186664a81f9
민경 <tjalsrud0101@gmail.com>
2024-02-19 11:20:06 +0900

    feat: question bookmark (#106)

--- src/main/java/com/peeroreum/exception/ExceptionType.java
+++ src/main/java/com/peeroreum/exception/ExceptionType.java
@@ -22,8 +22,10 @@ public enum ExceptionType {
     SELF_FOLLOW_WRONG_EXCEPTION(HttpStatus.BAD_REQUEST, "본인은 팔로우할 수 없습니다."),
     UPLOAD_FAILURE_EXCEPTION(HttpStatus.EXPECTATION_FAILED, "이미지 업로드에 실패했습니다."),
     ALREADY_LIKED(HttpStatus.CONFLICT, "이미 좋아요를 눌렀습니다."),
+    ALREADY_BOOKMARKED(HttpStatus.CONFLICT, "이미 북마크한 글입니다."),
     ALREADY_ENROLLED_WEDU(HttpStatus.CONFLICT, "이미 참여 중인 같이방입니다."),
     LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요 한 적 없는 글입니다."),
+    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크 한 적 없는 글입니다."),
     DO_NOT_HAVE_PERMISSION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
     CANNOT_CHANGE_NICKNAME(HttpStatus.NOT_ACCEPTABLE, "마지막 닉네임 변경 이후 30일이 지나지 않았습니다.");
 
