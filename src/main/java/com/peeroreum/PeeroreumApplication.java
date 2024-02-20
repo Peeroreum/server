089929eea060e7a0478d5ac457531870b21bf55c
민경 <tjalsrud0101@gmail.com>
2024-02-20 16:45:27 +0900

    fix: SdkClientException

--- src/main/java/com/peeroreum/PeeroreumApplication.java
+++ src/main/java/com/peeroreum/PeeroreumApplication.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+        }
+)
 @EnableJpaAuditing
 @EnableScheduling
 @EnableAsync
